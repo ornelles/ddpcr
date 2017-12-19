b4b0b38128b66850ee1c610e8c90bd5ada0d2d40
David Ornelles <ornelles@wakehealth.edu>
2017-12-19 15:27:14 -0500

    Allow upper case

--- R/well.info.R
+++ R/well.info.R
@@ -1,12 +1,14 @@
 #########################################################################################
 # well.info
 #
-# support function to parse character vector into numeric prefix, well, row and column,
-# return as a list with well reformatted according to sprintf format in "format".
+# support function to parse character vector into numeric prefix, well, row and
+# column, return as a list with well reformatted according to sprintf format
+# in "format" using upper or lower case as indicated in case
 #
 #########################################################################################
 
-well.info <- function(v, format="%02d") {
+well.info <- function(v, format = "%02d", case = c("upper", "lower")) {
+	case <- match.arg(case)
 	v <- tolower(as.character(v))
 	vv <- strsplit(v, "[[:alpha:]]+")					# separate prefix from column
 	row <- gsub("[[:digit:]]", "", v)					# keep only letters
@@ -16,7 +18,9 @@ well.info <- function(v, format="%02d") {
 		stop("bad row value")
 	if (any(column < 1 | column > 384))
 		stop("bad column value")
-	well <- paste(row, sprintf(format, column), sep="")
+	if (case == "upper")
+		row <- toupper(row)
+	well <- paste(row, sprintf(format, column), sep = "")
 	return(list(prefix = prefix, well = well, row = row,
 				column = as.character(column)))
 }
